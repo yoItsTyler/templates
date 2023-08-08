d2e193b897d497b54380c278519445d800b882d2
yoItsTyler <tyler@yoits.me>
2023-08-08 11:48:26 -0400

    testa

--- src/shared/helpers.js
+++ src/shared/helpers.js
@@ -24,3 +24,55 @@ export const uuid4 = () => {
     );
     return uuid;
   };
+
+
+  const onAddStudent = async () => {
+    if (authUser && studentName && parentEmail) {
+      try {
+        // Query Firestore to check if the email already exists within 'students'
+        const creativePlayDoc = await db.collection('CreativePlay').doc('members').get();
+        const studentsData = creativePlayDoc.data().students;
+  
+        // Find the student with the matching email
+        let existingStudent;
+        for (const studentId in studentsData) {
+          if (studentsData[studentId].parentEmail === parentEmail) {
+            existingStudent = studentsData[studentId];
+            break;
+          }
+        }
+  
+        if (existingStudent) {
+          // Email already exists, get the parentId of the existing student
+          const existingParentId = existingStudent.parentId;
+  
+          console.log('Email already exists in the database. Existing parent ID:', existingParentId);
+          // Now you can use 'existingParentId' in addStudentInfoToFirestore or any other needed logic
+          // For example: addStudentInfoToFirestore(existingParentId);
+          return;
+        }
+  
+        // Email does not exist, proceed with user creation
+        const data = {
+          uid: uid,
+          email: parentEmail,
+          password: 'randomPW',
+          displayName: studentName,
+          disabled: false,
+          role: 'member',
+        };
+        
+        const addEmployee = httpsCallable(functions, 'createUser');
+        addEmployee(data).then((result) => {
+          const emp = result.data;
+          console.log('Employee added, sign in:', emp, result.data);
+          addStudentInfoToFirestore(uid); // Use the 'uid' here
+          setStudentData(result);
+          setStudentFormOpen(false);
+        });
+      } catch (error) {
+        console.error('Error checking email:', error);
+      }
+    }
+  };
+  
